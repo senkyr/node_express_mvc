e6751b7810e328af6d2dee695057e7a981830c9d
Jakub Šenkýř <senkyr@spstrutnov.cz>
2021-03-12 11:28:19 +0100

    Základní routování

--- src/server.js
+++ src/server.js
@@ -6,13 +6,41 @@
 const hostname = 'localhost';
 const port = 8000;
 
+const path = require('path');
+
 const express = require('express');
+const session = require('express-session');
 
 const server = express();
 
-server.get('/', (dotaz, odpoved) => {
-    odpoved.send('Aplikace je ve výstavbě...');
+server.use(session({
+    secret: 'To je přísně tajná informace, Karen! Kdo vám to řekl?!',
+    secure: false,
+    resave: false,
+    saveUninitialized: false,
+}));
+
+// nemame indexovou stranku
+server.get(['/', 'index(.html)?'], (dotaz, odpoved) => {
+    if(dotaz.session.uzivatel == undefined)
+        odpoved.redirect('/prihlaseni.html');
+    else
+        odpoved.redirect('/profil.html');
 });
+
+// bez prihlaseni nelze nektere casti videt
+server.get(['/profil(.html)?', '/zruseni(.html)?'], (dotaz, odpoved) => {
+    if(dotaz.session.uzivatel == undefined)
+        odpoved.redirect('/prihlaseni.html')
+});
+
+// zkracene URL se doplni o koncovky souboru
+server.get(['/prihlaseni', '/registrace'], () => {
+    odpoved.redirect(dotaz.url + '.html');
+});
+
+// vsechno ostatni je pristupne jako staticke soubory
+server.use(express.static(path.join(__dirname, 'www')));
 
 server.listen(port, () => {
     console.log(`Server běží na http://${hostname}:${port}...`);
